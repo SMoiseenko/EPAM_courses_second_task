c3a8742a3d6cc5ee801e8faecc49b8a56d7dd353
S.Moiseenko <slipknotbiocom@gmail.com>
2018-11-07 23:13:50 +0300

    Remake constructor to autoparse and fill field.

--- src/main/java/by/moiseenko/text_entity/Text.java
+++ src/main/java/by/moiseenko/text_entity/Text.java
@@ -3,32 +3,74 @@ package by.moiseenko.text_entity;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+import org.apache.log4j.Logger;
+
 import by.moiseenko.text_services.FileReader;
+import by.moiseenko.text_services.TextParser;
 
-public class Text {
+public class Text implements CompositeTextParts {
 
-    private ArrayList<Paragraph> listOfParagraphs = new ArrayList<>();
+    private final static Logger logger = Logger.getLogger(Text.class);
 
-    private String tempText;
-/**
- * Default constructor that create new instance of the text and filled String tempText filed
- * by files with path
- * @param fileName
- */
+    private ArrayList<CompositeTextParts> listOfParagraphs = new ArrayList<>();
+
+    /**
+     * Default constructor that create new instance of the text, parse it, and
+     * filled listOfParagraphs
+     * 
+     * @param fileName
+     */
+
     public Text(String fileName) {
 	try {
-	    this.tempText = new FileReader().readFromFile(fileName);
+	    String tempText = new FileReader().readFromFile(fileName);
+	    this.listOfParagraphs = TextParser.parseText(tempText);
+
 	} catch (FileNotFoundException e) {
 	    System.out.println("File not found!!!");
 	}
     }
 
-    public ArrayList<Paragraph> getListOfParagraphs() {
+    public void getAllParagraphs() {
+	listOfParagraphs.stream().forEach(el -> System.out.printf("%-24s%s%n", el, el.returnAsString()));
+    }
+
+    public void getAllSentences() {
+	listOfParagraphs.stream().forEach(el -> (el).getListOfElements().stream()
+		.forEach(elm -> System.out.printf("%-24s%s%n", elm, elm.returnAsString())));
+    }
+
+    public void getAllWordsAndPuntcars() {
+	listOfParagraphs.stream().forEach(el -> (el).getListOfElements().stream()
+		.forEach(elm -> (elm).getListOfElements().stream().forEach(elem -> {
+		    System.out.printf("%-24s%s%n", elem, elem.returnAsString());
+		    logger.info(elem+ "   "+ elem.returnAsString());
+		})));
+    }
+
+    @Override
+    public void addElementToList(CompositeTextParts paragraph) {
+	listOfParagraphs.add(paragraph);
+    }
+
+    @Override
+    public ArrayList<CompositeTextParts> getListOfElements() {
 	return listOfParagraphs;
     }
 
-    public String getTestText() {
-	return tempText;
+    @Override
+    public String returnAsString() {
+	StringBuilder paragraph = new StringBuilder();
+	for (CompositeTextParts elements : listOfParagraphs) {
+	    paragraph.append(elements.returnAsString());
+	}
+	return String.valueOf(paragraph);
+    }
+
+    @Override
+    public String toString() {
+
+	return (String.valueOf(this.getClass())).substring(31) + ".class";
     }
 
 }
