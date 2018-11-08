60ba3539443c2a554bb35214e1f7d0daeca86e29
S.Moiseenko <slipknotbiocom@gmail.com>
2018-11-08 22:48:45 +0300

    Implement Comparable<Word>. @Override hashCode(), equals(), compareTo().

--- src/main/java/by/moiseenko/text_entity/Word.java
+++ src/main/java/by/moiseenko/text_entity/Word.java
@@ -2,7 +2,7 @@ package by.moiseenko.text_entity;
 
 import java.util.ArrayList;
 
-public class Word implements CompositeTextParts {
+public class Word implements CompositeTextParts, Comparable<Word> {
     private String word;
 
     public Word(String word) {
@@ -23,11 +23,42 @@ public class Word implements CompositeTextParts {
     @Override
     public void addElementToList(CompositeTextParts element) {
 	// TODO Auto-generated method stub
-	
+
     }
+
     @Override
     public String toString() {
-	
-	return (String.valueOf(this.getClass())).substring(31)+".class";
+	return (String.valueOf(this.getClass())).substring(31) + ".class";
     }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((word == null) ? 0 : word.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (!(obj instanceof Word))
+	    return false;
+	Word other = (Word) obj;
+	if (word == null) {
+	    if (other.word != null)
+		return false;
+	} else if (!word.equals(other.word))
+	    return false;
+	return true;
+    }
+
+    @Override
+    public int compareTo(Word word) {
+	return this.word.compareToIgnoreCase(word.returnAsString());
+    }
+
 }
