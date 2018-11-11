dbd56540e767e126242c0509eb2fa14afeba8f5f
S.Moiseenko <slipknotbiocom@gmail.com>
2018-11-11 16:02:33 +0300

    Only console output result of the task.

--- src/main/java/by/moiseenko/RunMeFirst.java
+++ src/main/java/by/moiseenko/RunMeFirst.java
@@ -1,24 +1,18 @@
 package by.moiseenko;
 
 import by.moiseenko.text_entity.Text;
-import by.moiseenko.text_services.FileReaderWriter;
 import by.moiseenko.text_services.TextManipulator;
-import by.moiseenko.text_services.TextRestorer;
-
+/**
+ * Class made for visualization (console output) result of the task.
+ * @author Sergei Moiseenko
+ * @see Text
+ * @see TextManipulator
+ */
 public class RunMeFirst {
 
     public static void main(String[] args) {
 
 	Text text = new Text("src/test/resources/1984.txt");
-	
-	TextManipulator textManipulator = new TextManipulator();
-	
-	//System.out.print(textManipulator.sortByAlphabet(text));
-	
-	//FileReaderWriter.writeToFile("log/result.txt", textManipulator.sortByAlphabet(text));
-	
-	System.out.println(TextRestorer.textRestorer(text));
-	
-	
+	System.out.print(new TextManipulator().sortByAlphabet(text));
     }
 }
