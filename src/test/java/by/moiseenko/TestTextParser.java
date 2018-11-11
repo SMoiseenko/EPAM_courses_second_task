a188382d926215774f6dcda1adbdd0d90dc60402
S.Moiseenko <slipknotbiocom@gmail.com>
2018-11-11 23:24:57 +0300

    First commit (class works correctly).

---
+++ src/test/java/by/moiseenko/TestTextParser.java
@@ -0,0 +1,34 @@
+package by.moiseenko;
+
+import java.util.ArrayList;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import by.moiseenko.text_entity.CompositeTextParts;
+import by.moiseenko.text_services.TextParser;
+/**
+* The Class to testing <i>textParser</i> method of {@link TextParser} class.
+* @author Sergei Moiseenko
+**/
+public class TestTextParser {
+
+    private String textParserString;
+    private ArrayList<CompositeTextParts> exepcedParserResult;
+    
+    @BeforeMethod
+    public void beforeMethod() {
+	InitDataForTests initData = new InitDataForTests();
+	exepcedParserResult = initData.getParagraphsList();
+	textParserString = initData.getTextForParsingString();
+    }
+    @Test
+    public void testTextParser() {
+	// WHEN
+	ArrayList<CompositeTextParts> actualParserResult = TextParser.textParser(textParserString);
+	// THEN
+	Assert.assertEquals(actualParserResult, exepcedParserResult);
+	    }
+    
+}
--- src/test/java/by/moiseenko/TextManipulatorTest.java
+++ src/test/java/by/moiseenko/TextManipulatorTest.java
@@ -1,5 +1,73 @@
 package by.moiseenko;
 
-public class TextManipulatorTest {
+import java.util.ArrayList;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import by.moiseenko.text_entity.CompositeTextParts;
+import by.moiseenko.text_entity.Text;
+import by.moiseenko.text_services.TextManipulator;
+/**
+ * The Class to testing all methods of {@link TextManipulator} class.
+ * @author Sergei Moiseenko
+ * @see TextManipulator
+ */
+public class TextManipulatorTest {
+    private Text testedText; 
+    private String expectedSortByAlphabetString;
+    private ArrayList<CompositeTextParts> expectedWordsList;
+    private ArrayList<CompositeTextParts> expectedWordsAndPunctcharsList;
+    private ArrayList<CompositeTextParts> expectedSentencesList;
+    private ArrayList<CompositeTextParts> expectedParagraphsList;
+
+    @BeforeClass
+    public void beforeClass() {
+	InitDataForTests initData = new InitDataForTests();
+	expectedSortByAlphabetString = initData.getSortByAlphabetString();
+	expectedWordsList = initData.getWordsList();
+	expectedWordsAndPunctcharsList = initData.getWordsAndPunctcharsList();
+	expectedSentencesList = initData.getSentencesList();
+	expectedParagraphsList = initData.getParagraphsList();
+	testedText = new Text("src/test/resources/FOR_TEST.txt");
+	
+    }
+
+    @Test
+    public void testSortByAlphabet() {
+	// WHEN
+	String actualSortByAlphabetString = TextManipulator.sortByAlphabet(testedText);
+	// THEN
+	Assert.assertEquals(actualSortByAlphabetString, expectedSortByAlphabetString);
+    }
+    @Test
+    public void testGetAllWords() {
+	// WHEN
+	ArrayList<CompositeTextParts> actualWordsList = TextManipulator.getAllWords(testedText);
+	// THEN
+	Assert.assertEquals(actualWordsList, expectedWordsList);
+    }
+    @Test
+    public void testGetAllWordsAndPunctchars() {
+	// WHEN
+	ArrayList<CompositeTextParts> actualWordsAndPunctcharsList = TextManipulator.getAllWordsAndPunctchars(testedText);
+	//THEN
+	Assert.assertEquals(actualWordsAndPunctcharsList, expectedWordsAndPunctcharsList);
+    }
+    @Test
+    public void testGetAllSentences() {
+	// WHEN
+	ArrayList<CompositeTextParts> actualSentencesList = TextManipulator.getAllSentences(testedText);
+	// THEN
+	Assert.assertEquals(actualSentencesList, expectedSentencesList);
+    }
+    @Test
+    public void testGetAllParagraphs() {
+	// WHEN
+	ArrayList<CompositeTextParts> actualParagraphsList = TextManipulator.getAllParagraphs(testedText);
+	// THEN
+	Assert.assertEquals(actualParagraphsList, expectedParagraphsList);
+    }
+    
 }
