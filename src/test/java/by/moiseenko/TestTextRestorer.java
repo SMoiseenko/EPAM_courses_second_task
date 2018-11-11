ec2ccf03c8b0b435c3b2d32510048ea6cab2f0be
S.Moiseenko <slipknotbiocom@gmail.com>
2018-11-11 23:50:04 +0300

    First commit (class works correctly).

---
+++ src/test/java/by/moiseenko/TestTextRestorer.java
@@ -0,0 +1,30 @@
+package by.moiseenko;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import by.moiseenko.text_entity.Text;
+import by.moiseenko.text_services.TextRestorer;
+
+public class TestTextRestorer {
+    private Text testedText;
+    private String exepedTextRestoredString;
+    
+    @BeforeMethod
+    public void beforeMethod() {
+	InitDataForTests initData = new InitDataForTests();
+	exepedTextRestoredString = initData.getTextForParsingString();
+	testedText = new Text("src/test/resources/FOR_TEST.txt");
+	
+    }
+    
+    @Test
+    public void testTextRestorer() {
+	// WHEN
+	String actualTextRestoredString = TextRestorer.textRestorer(testedText);
+	// THEN
+	Assert.assertEquals(actualTextRestoredString, exepedTextRestoredString);
+    }
+        
+}
