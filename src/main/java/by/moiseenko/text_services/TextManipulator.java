a413afd7e0a3bd9a889168b3c59a6cff0669cbff
moiseenko-s <moiseenko-s@MOISEENKO-S>
2018-11-12 16:25:30 +0300

    Add logger and javadoc.

--- src/main/java/by/moiseenko/text_services/TextManipulator.java
+++ src/main/java/by/moiseenko/text_services/TextManipulator.java
@@ -5,40 +5,76 @@ package by.moiseenko.text_services;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
+
+import org.apache.log4j.Logger;
+
 import by.moiseenko.text_entity.CompositeTextParts;
 import by.moiseenko.text_entity.Text;
 import by.moiseenko.text_entity.Word;
 
 /**
- * The Class TextManipulator.
+ * The utility class to work with {@link Text} objects by methods: <br>
+ * </i>ArrayList<CompositeTextParts> getAllParagraphs(Text text)</i> <br>
+ * </i>ArrayList<CompositeTextParts> getAllSentences(Text text)</i> <br>
+ * </i>ArrayList<CompositeTextParts> getAllWordsAndPunctchars(Text text)</i>
+ * <br>
+ * </i>ArrayList<CompositeTextParts> getAllWords(Text text)</i> <br>
+ * </i>String sortByAlphabet(Text text)</i>
+ * 
+ * @author moiseenko-s
+ * @see Text
  */
 public class TextManipulator {
+    private final static Logger logger = Logger.getLogger(TextManipulator.class);
 
     private TextManipulator() {
 
     }
 
     public static ArrayList<CompositeTextParts> getAllParagraphs(Text text) {
+	if (logger.isInfoEnabled()) {
+	    StringBuffer forText = new StringBuffer();
+	    text.getListOfElements().stream().forEach(el -> forText.append(el.returnAsString() + "\n"));
+	    logger.info(
+		    "Text object\n" + forText.toString() + "\n consist with next paragraphs\n" + forText.toString());
+	}
 	return text.getListOfElements();
     }
 
     public static ArrayList<CompositeTextParts> getAllSentences(Text text) {
 	ArrayList<CompositeTextParts> allSentenses = new ArrayList<>();
 	getAllParagraphs(text).stream().forEach(par -> allSentenses.addAll(par.getListOfElements()));
+	if (logger.isInfoEnabled()) {
+	    StringBuffer forText = new StringBuffer();
+	    StringBuffer forSentences = new StringBuffer();
+	    text.getListOfElements().stream().forEach(el -> forText.append(el.returnAsString() + "\n"));
+	    allSentenses.stream().forEach(el -> forSentences.append(el.returnAsString() + "\n"));
+	    logger.info("Text object\n" + forText.toString() + "\n consist with next sentences\n"
+		    + forSentences.toString());
+	}
 	return allSentenses;
+
     }
-//tested
+
     public static ArrayList<CompositeTextParts> getAllWordsAndPunctchars(Text text) {
 	ArrayList<CompositeTextParts> allWordsAndPunctchars = new ArrayList<>();
 	getAllSentences(text).stream().forEach(sen -> allWordsAndPunctchars.addAll(sen.getListOfElements()));
+	if (logger.isInfoEnabled()) {
+	    StringBuffer forText = new StringBuffer();
+	    StringBuffer forWordsAndPunctchars = new StringBuffer();
+	    text.getListOfElements().stream().forEach(el -> forText.append(el.returnAsString() + "\n"));
+	    allWordsAndPunctchars.stream().forEach(el -> forWordsAndPunctchars.append(el.returnAsString() + "\n"));
+	    logger.info("Text object\n" + forText.toString() + "\n consist with next sentences\n"
+		    + forWordsAndPunctchars.toString());
+	}
 	return allWordsAndPunctchars;
     }
-//tested
+
     public static ArrayList<CompositeTextParts> getAllWords(Text text) {
 	return getAllWordsAndPunctchars(text).stream().filter(el -> el instanceof Word)
 		.collect(Collectors.toCollection(ArrayList::new));
     }
-//tested
+
     public static String sortByAlphabet(Text text) {
 
 	StringBuilder resultString = new StringBuilder();
@@ -59,6 +95,12 @@ public class TextManipulator {
 	}
 	resultString.delete(0, 2);
 	resultString.append(";");
+	if (logger.isInfoEnabled()) {
+	    StringBuffer forText = new StringBuffer();
+	    text.getListOfElements().stream().forEach(el -> forText.append(el.returnAsString() + "\n"));
+	    logger.info("Text object\n" + forText.toString() + "\n after method \"sortByAlphabet\" return next string\n"
+		    + resultString.toString());
+	}
 	return String.valueOf(resultString);
     }
 }
