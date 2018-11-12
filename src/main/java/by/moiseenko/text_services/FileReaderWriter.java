9fd5244444df970131ffc5c442d34a40e8ddec90
moiseenko-s <moiseenko-s@MOISEENKO-S>
2018-11-12 15:32:12 +0300

    Add logger and javadoc.

--- src/main/java/by/moiseenko/text_services/FileReaderWriter.java
+++ src/main/java/by/moiseenko/text_services/FileReaderWriter.java
@@ -6,17 +6,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
 /**
- * Utility class that receive path to file and return it as String (maybe make
- * inner?s)
- * 
+ * Utility class that:
+ * <br>read from <b>txt</b> file to String with <i>String readFromFile(String fileName)</i> method;
+ * <br> write to <b>txt</b> file from String with <i>void writeToFile(String path, String text)</i> method;
  * @author Sergei Moiseenko
- *
  */
 public class FileReaderWriter {
-    
+    private final static Logger logger = Logger.getLogger(FileReaderWriter.class);
+
     private FileReaderWriter() {
-	
+
     }
 
     public static String readFromFile(String fileName) {
@@ -26,9 +28,12 @@ public class FileReaderWriter {
 		str.append("\n" + in.nextLine());
 	    }
 	} catch (FileNotFoundException e) {
-	    System.out.println("File not found!!!");
+	    logger.error("File" + fileName + " not found!!!");
 	}
 	str.deleteCharAt(0);
+	if (logger.isInfoEnabled()) {
+	    logger.info("File" + fileName + "was read in String \n" + str.toString());
+	}
 	return str.toString();
     }
 
@@ -36,11 +41,14 @@ public class FileReaderWriter {
 	try (FileWriter writer = new FileWriter(path)) {
 	    writer.write(text);
 	    writer.flush();
+	    if (logger.isInfoEnabled()) {
+		    logger.info("String \n" + text + "\n was written to the file" + path);
+	    }
 	    System.out.println("File was written successfully.");
 
 	} catch (IOException e) {
-	    System.out.println(
-		    "File named exists but is a directory rather than a regular file, does not exist but cannot be created, or cannot be opened for any other reason");
+	    logger.error("File" + path
+		    + "exists but is a directory rather than a regular file, does not exist but cannot be created, or cannot be opened for any other reason");
 	}
 
     }
